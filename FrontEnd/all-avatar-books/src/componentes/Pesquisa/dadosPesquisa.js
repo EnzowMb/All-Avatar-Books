3f006a2441c568640b5e9ba6b8e0449c289ca179
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-08-01 19:10:34 -0300

    Simulando um banco de dados

---
+++ FrontEnd/all-avatar-books/src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+//Simulação do Banco de dados
+
+import hq from '../../imagens/The Promise pt1.jpg'
+
+export const HQS = [
+    {nome: 'Team Avatar Tales', id: 1, src: hq},
+    {nome: 'Katara and the Pirates Silver', id: 2, src: hq},
+    {nome: 'Suki, Alone', id: 3, src: hq},
+    {nome: 'The Promise', id: 4, src: hq}
+]
--- FrontEnd/all-avatar-books/src/componentes/Pesquisa/index.js
+++ FrontEnd/all-avatar-books/src/componentes/Pesquisa/index.js
@@ -1,19 +1,33 @@
 import { useState } from 'react'
 import Input from '../Input'
 import './estilo.css'
+import { HQS } from './dadosPesquisa'
 
 const Pesquisa = () => {
 
-    const [textoDigitado, setTextoDigitado] = useState('')
+    const [HQPesquisadas, setHQPesquisadas] = useState([])
 
     return (
         <section className='PesquisaContainer'>
             <h2 className='Titulo'>Pesquisar HQ</h2>
             <Input
                 placeholder='Encontre sua HQ favorita!'
-                onBlur={evento => setTextoDigitado(evento.target.value)}
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = HQS.filter(hq => {
+                        if (textoDigitado !== '') {
+                            return hq.nome.includes(textoDigitado)
+                        }
+                        })
+                setHQPesquisadas(resultadoPesquisa)
+                }}
             />
-            <p>{textoDigitado}</p>
+            {HQPesquisadas.map(hq => (
+                <div className='resultado'>
+                    <p><strong>{hq.nome}</strong></p>
+                    <img src={hq.src} />
+                </div>
+            ))}
         </section>
     )
 }
