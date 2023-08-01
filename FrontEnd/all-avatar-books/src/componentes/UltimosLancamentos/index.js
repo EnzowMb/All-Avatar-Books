4002dc23977bcf29d93c02ac6dfa37a689a6e151
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-08-01 19:32:40 -0300

    Estilizando o titulo e adicionando props

--- FrontEnd/all-avatar-books/src/componentes/UltimosLancamentos/index.js
+++ FrontEnd/all-avatar-books/src/componentes/UltimosLancamentos/index.js
@@ -1,10 +1,14 @@
+import { Titulo } from "../Titulo"
 import { HQS } from "./dadosUltimosLancamentos"
 import './estilo.css'
 
 const UltimosLancamentos = () => {
     return (
         <section className="ultimos">
-            <h2 className="titulo" >Ultimos Lancamentos</h2>
+            <Titulo 
+            cor={"#000"}
+            tamanhoFonte={"36px"}
+            >Ultimos Lancamentos</Titulo>
             <div className="novos" >
             {HQS.map(hq => (
                 <img src={hq.src} />
