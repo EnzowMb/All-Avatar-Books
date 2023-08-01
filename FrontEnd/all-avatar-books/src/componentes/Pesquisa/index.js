31785df73aa23b067e25d7caf7bdacb9ed72172f
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-08-01 12:31:01 -0300

    Criando componente Pesquisa

---
+++ FrontEnd/all-avatar-books/src/componentes/Pesquisa/index.js
@@ -0,0 +1,14 @@
+import './estilo.css'
+
+const Pesquisa = () => {
+    return (
+        <section >
+            <h2>Pesquisar HQ</h2>
+            <input 
+                placeholder='Encontre sua HQ'
+            />
+        </section>
+    )
+}
+
+export default Pesquisa
