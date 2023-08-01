cf4e59821d94dc44118df73f853d94fa8c4a65f6
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-08-01 11:33:55 -0300

    Adicionando CSS para IconesHeader

--- FrontEnd/all-avatar-books/src/componentes/IconesHeader/index.js
+++ FrontEnd/all-avatar-books/src/componentes/IconesHeader/index.js
@@ -2,6 +2,7 @@ import toph from '../../imagens/Toph.png'
 import katara from '../../imagens/Katara.png'
 import aang from '../../imagens/Aang.png'
 import sokka from '../../imagens/Sokka.png'
+import './estilo.css'
 
 const icones = [toph, katara, aang, sokka]
 
