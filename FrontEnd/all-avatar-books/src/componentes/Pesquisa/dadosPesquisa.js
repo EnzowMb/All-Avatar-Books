317e772cfac7dba3b3c30147e7279ab766ae85b5
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-08-01 21:53:51 -0300

    Fixando bug do componente Pesquisa

--- FrontEnd/all-avatar-books/src/componentes/Pesquisa/dadosPesquisa.js
+++ FrontEnd/all-avatar-books/src/componentes/Pesquisa/dadosPesquisa.js
@@ -6,5 +6,8 @@ export const HQS = [
     {nome: 'Team Avatar Tales', id: 1, src: hq},
     {nome: 'Katara and the Pirates Silver', id: 2, src: hq},
     {nome: 'Suki, Alone', id: 3, src: hq},
-    {nome: 'The Promise', id: 4, src: hq}
+    {nome: 'The Promise', id: 4, src: hq},
+    {nome: 'The Search', id: 5, src: hq},
+    {nome: 'The Rift', id: 6, src: hq},
+    {nome: 'Toph Beifongs Metalbending Academy', id: 7, src: hq}
 ]
--- FrontEnd/all-avatar-books/src/componentes/Pesquisa/index.js
+++ FrontEnd/all-avatar-books/src/componentes/Pesquisa/index.js
@@ -2,33 +2,53 @@ import { useState } from 'react'
 import Input from '../Input'
 import './estilo.css'
 import { HQS } from './dadosPesquisa'
+import { styled } from 'styled-components'
+
+const PesquisaContainer = styled.section`
+    background-image: linear-gradient(90deg, #5AACCF 55%, #c6b3b0 110%);
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: ${props => props.tamanho};
+    width: 100%;
+`
+
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+`
 
 const Pesquisa = () => {
 
     const [HQPesquisadas, setHQPesquisadas] = useState([])
+    const [tamanhoPesquisa, setTamanhoPesquisa] = useState('')
 
     return (
-        <section className='PesquisaContainer'>
+        <PesquisaContainer tamanho={{tamanhoPesquisa} * 30 }>
             <h2 className='Titulo'>Pesquisar HQ</h2>
             <Input
                 placeholder='Encontre sua HQ favorita!'
                 onBlur={evento => {
                     const textoDigitado = evento.target.value
                     const resultadoPesquisa = HQS.filter(hq => {
-                        if (textoDigitado !== '') {
+                        if(textoDigitado !== '') {
                             return hq.nome.includes(textoDigitado)
                         }
-                        })
-                setHQPesquisadas(resultadoPesquisa)
+                    })
+                    setHQPesquisadas(resultadoPesquisa)
+                    setTamanhoPesquisa(resultadoPesquisa.length)
                 }}
             />
             {HQPesquisadas.map(hq => (
-                <div className='resultado'>
+                <Resultado>
                     <p><strong>{hq.nome}</strong></p>
                     <img src={hq.src} />
-                </div>
+                </Resultado>
             ))}
-        </section>
+        </PesquisaContainer>
     )
 }
 
